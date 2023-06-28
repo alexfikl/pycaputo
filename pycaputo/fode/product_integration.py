0a56c4346457d491df8fa052bba022b446354cee
Alexandru Fikl <alexfikl@gmail.com>
2023-06-28 21:28:27 +0300

    fode: add missing __future__ import

--- pycaputo/fode/product_integration.py
+++ pycaputo/fode/product_integration.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: 2023 Alexandru Fikl <alexfikl@gmail.com>
 # SPDX-License-Identifier: MIT
 
+from __future__ import annotations
+
 from dataclasses import dataclass
 from functools import cached_property
 from typing import Iterator
