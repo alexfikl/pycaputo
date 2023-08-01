66af92db3f2be8d59536b42c3e56b7bbfab9bdfe
Alexandru Fikl <alexfikl@gmail.com>
2023-08-01 20:59:25 +0300

    lipschitz: add tests

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -8,8 +8,8 @@ import pathlib
 import numpy as np
 import pytest
 
-from pycaputo.logging import get_logger
-from pycaputo.utils import set_recommended_matplotlib
+from pycaputo.logging import get_logger, stringify_table
+from pycaputo.utils import Array, ScalarFunction, set_recommended_matplotlib
 
 logger = get_logger("pycaputo.test_misc")
 dirname = pathlib.Path(__file__).parent
@@ -27,7 +27,8 @@ def test_lipschitz_uniform_sample(*, visualize: bool = False) -> None:
     n = 128
     delta = 0.0625 * np.sqrt(2) * (b - a)
 
-    x, y = uniform_diagonal_sample(a, b, n, delta=delta, rng=np.random.default_rng())
+    rng = np.random.default_rng(seed=42)
+    x, y = uniform_diagonal_sample(a, b, n, delta=delta, rng=rng)
     assert np.all(np.abs(x - y) <= delta)
     assert np.all(np.logical_and(a <= x, x <= b))
     assert np.all(np.logical_and(a <= y, y <= b))
@@ -51,6 +52,70 @@ def test_lipschitz_uniform_sample(*, visualize: bool = False) -> None:
         ax.set_ylim([a - delta, b + delta])
         ax.set_xlabel("$x$")
         ax.set_ylabel("$y$")
+
+
+# }}}
+
+
+# {{{ test_estimate_lischitz_constant
+
+
+def f_wood_1(x: Array) -> Array:
+    return np.array(x - x**3 / 3)
+
+
+def f_wood_2(x: Array) -> Array:
+    return np.array(np.sin(x) + np.sin(2 * x / 3))
+
+
+def f_wood_3(x: Array) -> Array:
+    return -np.array(np.sum([np.sin((i + 1) * x + i) for i in range(1, 6)]))
+
+
+@pytest.mark.parametrize(
+    ("f", "L", "a", "b"),
+    [
+        (f_wood_1, 1.0, -1.0, 1.0),
+        (f_wood_2, 1.67, 3.14, 20.4),
+        (f_wood_3, 67, -10.0, 10.0),
+    ],
+)
+def test_estimate_lischitz_constant(
+    f: ScalarFunction,
+    L: float,
+    a: float,
+    b: float,
+    *,
+    visualize: bool = True,
+) -> None:
+    nbatches = [25, 50, 75, 100]
+    nslopes = [3, 5, 7, 9, 11]
+    delta = 0.05
+
+    from rich.table import Table
+
+    rng = np.random.default_rng(seed=42)
+    table = Table(f"L = {L}", *[f"m = {m}" for m in nbatches])
+    lipschitz_approx = np.empty(len(nbatches))
+
+    from pycaputo.lipschitz import estimate_lipschitz_constant
+
+    for n in nslopes:
+        for i, m in enumerate(nbatches):
+            lipschitz_approx[i] = estimate_lipschitz_constant(
+                f,
+                a,
+                b,
+                delta=delta,
+                nslopes=n,
+                nbatches=m,
+                rng=rng,
+            )
+            logger.info("n %3d m %3d L %.5e Lapprox %.5e", n, m, L, lipschitz_approx[i])
+
+        table.add_row(f"n = {n}", *[f"{Lapprox:.5e}" for Lapprox in lipschitz_approx])
+
+    logger.info("Results:\n%s", stringify_table(table))
 
 
 # }}}
