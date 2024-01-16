f80a32ad0436456c4eee7692a9a7358f3a93cbd5
Alexandru Fikl <alexfikl@gmail.com>
2024-01-16 19:32:31 +0200

    feat: implement EIF model

---
+++ examples/integrate-and-fire-eif.py
@@ -0,0 +1,148 @@
+# SPDX-FileCopyrightText: 2023 Alexandru Fikl <alexfikl@gmail.com>
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+import numpy as np
+
+from pycaputo.integrate_fire import eif
+from pycaputo.logging import get_logger
+
+logger = get_logger("integrate-and-fire")
+
+# {{{ model
+
+# time interval
+tstart, tfinal = 0.0, 10.0
+# fractional order
+alpha = 0.95
+
+param = eif.EIFDim(
+    current=160,
+    C=100,
+    gl=12.0,
+    e_leak=-60.0,
+    delta_t=2.0,
+    vt=-50.0,
+    v_reset=-48,
+    v_peak=0.0,
+)
+model = eif.EIFModel(param.nondim(alpha))
+
+logger.info("Parameters:\n%s", param)
+logger.info("Parameters:\n%s", model.param)
+
+# }}}
+
+# {{{ setup
+
+from pycaputo.controller import make_jannelli_controller
+
+# initial condition
+rng = np.random.default_rng()
+y0 = np.array([rng.uniform(model.param.v_reset - 2.0, model.param.v_reset)])
+
+dtinit = 1.0e-3
+c = make_jannelli_controller(
+    tstart,
+    tfinal,
+    dtmin=1.0e-5,
+    chimin=0.01,
+    chimax=0.1,
+    abstol=1.0e-4,
+)
+
+stepper = eif.CaputoExponentialIntegrateFireL1Method(
+    derivative_order=(alpha,),
+    control=c,
+    y0=(y0,),
+    source=model.source,
+    model=model,
+)
+
+# }}}
+
+# {{{ evolution
+
+from pycaputo.integrate_fire import StepAccepted, StepRejected, evolve
+
+ts = []
+ys = []
+spikes = []
+eests = []
+
+for event in evolve(stepper, dtinit=dtinit):
+    if isinstance(event, StepAccepted):
+        ts.append(event.t)
+        ys.append(event.y)
+        eests.append(event.eest)
+
+        if event.spiked:
+            spikes.append(event.iteration)
+    elif isinstance(event, StepRejected):
+        pass
+    else:
+        raise RuntimeError("Evolution failed!")
+
+    logger.info(
+        "[%06d] t = %.5e dt = %.5e energy %.5e eest %+.5e",
+        event.iteration,
+        event.t,
+        event.dt,
+        np.linalg.norm(event.y),
+        event.eest,
+    )
+
+# }}}
+
+# {{{ plotting
+
+try:
+    import matplotlib  # noqa: F401
+except ImportError as exc:
+    raise SystemExit(0) from exc
+
+from pycaputo.utils import figure, set_recommended_matplotlib
+
+set_recommended_matplotlib()
+t = np.array(ts)
+s = np.array(spikes)
+
+y = np.array(ys).squeeze()
+eest = np.array(eests)
+
+# dimensionalize variables
+t = t / model.param.T
+y = param.delta_t * y + param.vt
+
+with figure("integrate-fire-eif") as fig:
+    ax = fig.gca()
+
+    ax.plot(t, y, lw=3)
+    ax.axhline(param.v_peak, color="k", ls="-")
+    ax.axhline(param.v_reset, color="k", ls="--")
+    ax.plot(t[s], y[s], "ro")
+
+    ax.set_xlabel("$t$ (ms)")
+    ax.set_ylabel("$V$ (mV)")
+
+with figure("integrate-fire-eif-dt") as fig:
+    ax = fig.gca()
+
+    ax.semilogy(t[:-1], np.diff(t))
+    ax.axhline(dtinit, color="k", ls="--")
+    ax.axhline(c.dtmin, color="k", ls="--")
+    ax.set_xlabel("$t$ (ms)")
+    ax.set_ylabel(r"$\Delta t$ (ms)")
+
+with figure("integrate-fire-eif-eest") as fig:
+    ax = fig.gca()
+
+    ax.plot(t, eest)
+    ax.axhline(1.0, color="k", ls="--")
+    ax.axhline(0.0, color="k", ls="--")
+    ax.plot(t[s], eest[s], "ro")
+    ax.set_xlabel("$t$ (ms)")
+    ax.set_ylabel("$E_{est}$")
+
+# }}}
---
+++ src/pycaputo/integrate_fire/eif.py
@@ -0,0 +1,262 @@
+# SPDX-FileCopyrightText: 2024 Alexandru Fikl <alexfikl@gmail.com>
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import NamedTuple
+
+import numpy as np
+
+from pycaputo.fode.base import advance
+from pycaputo.history import ProductIntegrationHistory
+from pycaputo.integrate_fire.base import (
+    AdvanceResult,
+    CaputoIntegrateFireL1Method,
+    IntegrateFireModel,
+)
+from pycaputo.logging import get_logger
+from pycaputo.utils import Array, dc_stringify
+
+logger = get_logger(__name__)
+
+# {{{ model
+
+
+class EIFDim(NamedTuple):
+    """Dimensional parameters for the Exponential Integrate-and-Fire (EIF) model."""
+
+    #: Added current :math:`I` (in picoamperes *pA*).
+    current: float
+    #: Total capacitance :math:`C` (in picofarad per millisecond *pF / ms^(alpha - 1)*).
+    C: float
+    #: Total leak conductance :math:`g_L` (in nanosiemens *nS*).
+    gl: float
+    #: Equilibrium potential leak :math:`E_L` (in microvolts *mV*).
+    e_leak: float
+    #: Threshold slope factor :math:`\Delta_T` (in microvolts *mV*).
+    delta_t: float
+    #: Effective threshold potential :math:`V_T` (in microvolts *mV*).
+    vt: float
+
+    #: Peak potential :math:`V_{peak}` (in millivolts: *mV*).
+    v_peak: float
+    #: Reset potential :math:`V_r` (in millivolts: *mV*).
+    v_reset: float
+
+    def __str__(self) -> str:
+        return dc_stringify(
+            {
+                "I      (current / pA)": self.current,
+                "C      (total capacitance / pF/ms^alpha)": self.C,
+                "g_L    (total leak conductance / nS)": self.gl,
+                "E_leak (equilibrium potential leak / mV)": self.e_leak,
+                "delta_T(threshold slope factor / mV)": self.delta_t,
+                "V_T    (effective threshold potential / mV)": self.vt,
+                "V_peak (peak potential / mV)": self.v_peak,
+                "V_r    (reset potential / mV)": self.v_reset,
+            },
+            header=("model", type(self).__name__),
+        )
+
+    def nondim(self, alpha: float) -> EIF:
+        r"""Construct a non-dimensional set of parameters for the EIF model.
+
+        The non-dimensionalization is performed using the following rescaling
+
+        .. math::
+
+            \hat{t} = \sqrt[\alpha]{\frac{g_L}{C}} t,
+            \qquad
+            \hat{V} = \frac{V - V_T}{\Delta_T},
+            \qquad
+            \hat{I} = \frac{I}{g_L \Delta_T},
+
+        which results in a reduction of the parameter space to just the threshold
+        values :math:`(V_{peak}, V_r)` and the constants :math:`(I, E_L)`.
+
+        :arg alpha: the order of the fractional derivative.
+        """
+        return EIF(
+            alpha=alpha,
+            T=(self.gl / self.C) ** (1 / alpha),
+            current=self.current / (self.gl * self.delta_t),
+            e_leak=(self.e_leak - self.vt) / self.delta_t,
+            v_peak=(self.v_peak - self.vt) / self.delta_t,
+            v_reset=(self.v_reset - self.vt) / self.delta_t,
+        )
+
+
+class EIF(NamedTuple):
+    """Non-dimensional parameters for the EIF model (see :class:`EIFDim`)."""
+
+    #: Fractional order used in the non-dimensionalization.
+    alpha: float
+    #: Fractional time scale.
+    T: float
+
+    #: Current :math:`I`.
+    current: float
+    #: Equilibrium potential leak :math:`E_L`.
+    e_leak: float
+
+    #: Peak potential :math:`V_{peak}`.
+    v_peak: float
+    #: Reset potential :math:`V_r`.
+    v_reset: float
+
+    def __str__(self) -> str:
+        return dc_stringify(
+            {
+                "alpha  (fractional order)": self.alpha,
+                "T      (fractional time scale)": self.T,
+                "I      (current)": self.current,
+                "E_leak (equilibrium potential leak)": self.e_leak,
+                "V_peak (peak potential)": self.v_peak,
+                "V_r    (reset potential)": self.v_reset,
+            },
+            header=("model", type(self).__name__),
+        )
+
+
+@dataclass(frozen=True)
+class EIFModel(IntegrateFireModel):
+    r"""Functionals for the EIF model with parameters :class:`EIF`.
+
+    .. math::
+
+        D_C^\alpha[V](t) = I(t) - (V - E_L) + \exp V,
+
+    where :math:`I` is taken to be a constant by default, but can be
+    easily overwritten by subclasses. The reset condition is given by
+
+    .. math::
+
+        \text{if } V > V_{peak} \qquad \text{then} \qquad V \gets V_r.
+    """
+
+    #: Non-dimensional parameters of the model.
+    param: EIF
+
+    if __debug__:
+
+        def __post_init__(self) -> None:
+            if not isinstance(self.param, EIF):
+                raise TypeError(
+                    f"Invalid parameter type: '{type(self.param).__name__}'"
+                )
+
+    def source(self, t: float, y: Array) -> Array:
+        """Evaluate right-hand side of the LIF model."""
+        return np.array(self.param.current - (y - self.param.e_leak) + np.exp(y))
+
+    def source_jac(self, t: float, y: Array) -> Array:
+        """Evaluate the Jacobian of the right-hand side of the LIF model."""
+        return -1.0 + np.exp(y)
+
+    def spiked(self, t: float, y: Array) -> float:
+        """Compute a delta from the peak threshold :math:`V_{peak}`.
+
+        :returns: a delta of :math:`V - V_{peak}` that can be used to determine
+            if the neuron spiked.
+        """
+
+        (V,) = np.real_if_close(y)
+        return float(V - self.param.v_peak)
+
+    def reset(self, t: float, y: Array) -> Array:
+        """Evaluate the reset values for the LIF model.
+
+        This function assumes that the neuron has spiked, i.e. that :meth:`spiked`
+        returns a non-negative value. If this is not the case, the reset should
+        not be applied.
+        """
+        # TODO: should this be a proper check?
+        assert self.spiked(t, y) > -10.0 * np.finfo(y.dtype).eps
+
+        return np.full_like(y, self.param.v_reset)
+
+
+# }}}
+
+
+# {{{ CaputoExponentialIntegrateFireL1Method
+
+
+@dataclass(frozen=True)
+class CaputoExponentialIntegrateFireL1Method(CaputoIntegrateFireL1Method[EIFModel]):
+    r"""Implementation of the L1 method for the Exponential Integrate-and-Fire model.
+
+    The model is described by :class:`EIFModel` with parameters :class:`EIF`.
+    """
+
+    model: EIFModel
+
+    def solve(self, t: float, y0: Array, c: Array, r: Array) -> Array:
+        r"""Solve the implicit equation for the EIF model.
+
+        In this case, since the right-hand side is nonlinear, but we can solve
+        the implicit equation using the Lambert W function, a special function
+        that is a solution to
+
+        .. math::
+
+            z = w e^w.
+        """
+        from scipy.special import lambertw
+
+        d0 = 1 + c
+        d1 = c
+        # NOTE: we need the terms that do not depend on V, so we do
+        #   f(t, y) + y - exp(y) = I(t) + E_L
+        # since the current may be time-dependent
+        d2 = r + c * (self.source(t, y0) + y0 - np.exp(y0))
+
+        V = d2 / d0 - lambertw(-d1 / d0 * np.exp(d2 / d0), tol=1.0e-12)
+        V = np.real_if_close(V, tol=100)
+
+        assert np.linalg.norm(V - c * self.source(t, V) - r) < 1.0e-8
+
+        return np.array(V)
+
+
+@advance.register(CaputoExponentialIntegrateFireL1Method)
+def _advance_caputo_eif_l1(
+    m: CaputoExponentialIntegrateFireL1Method,
+    history: ProductIntegrationHistory,
+    y: Array,
+    dt: float,
+) -> AdvanceResult:
+    from pycaputo.integrate_fire.base import (
+        advance_caputo_integrate_fire_l1,
+        advance_caputo_integrate_fire_spike_exp,
+    )
+
+    tprev = history.current_time
+    t = tprev + dt
+
+    result = advance_caputo_integrate_fire_l1(m, history, y, dt)
+
+    is_complex = np.any(np.iscomplex(result.y))
+    if is_complex or m.model.spiked(t, result.y) > 0.0:
+        p = m.model.param
+        result = advance_caputo_integrate_fire_spike_exp(
+            tprev, y, t, result, v_peak=p.v_peak, v_reset=p.v_reset
+        )
+
+        from pycaputo.controller import AdaptiveController
+
+        if is_complex and isinstance(m.control, AdaptiveController):
+            c = m.control
+
+            if dt < c.dtmin or c.nrejects > c.max_rejects:
+                # NOTE: we really tried to reduce the time step until the spike
+                # occurred, so now it's time to give up and spike!
+                pass
+            else:
+                result = result._replace(spiked=np.array(0))
+
+    return result
+
+
+# }}}
