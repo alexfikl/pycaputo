bd3f19ac464342bcf27f9bded0c69974121981fe
Alexandru Fikl <alexfikl@gmail.com>
2024-01-30 18:35:12 +0200

    test: test mittagleffler against Mathematica results

--- tests/test_mittag_leffler.py
+++ tests/test_mittag_leffler.py
@@ -4,9 +4,15 @@
 import numpy as np
 import pytest
 
+from pycaputo import mittagleffler as ml
 from pycaputo.logging import get_logger
+from pycaputo.utils import set_recommended_matplotlib
 
 logger = get_logger("pycaputo.test_mittag_leffler")
+set_recommended_matplotlib()
+
+
+# {{{ test_mittag_leffler_series
 
 
 @pytest.mark.parametrize(
@@ -24,19 +30,23 @@ logger = get_logger("pycaputo.test_mittag_leffler")
     ],
 )
 def test_mittag_leffler_series(alpha: float, beta: float) -> None:
-    from pycaputo.mittagleffler import mittag_leffler, mittag_leffler_series
-
     rng = np.random.default_rng(seed=42)
     z = rng.random(128)
 
-    result_ref = mittag_leffler(z, alpha=alpha, beta=beta)
+    result_ref = ml.mittag_leffler(z, alpha=alpha, beta=beta)
     result = np.vectorize(
-        lambda zi: 0j + mittag_leffler_series(zi, alpha=alpha, beta=beta)
+        lambda zi: 0j + ml.mittag_leffler_series(zi, alpha=alpha, beta=beta)
     )(z)
 
     error = np.linalg.norm(result - result_ref) / (1 + np.linalg.norm(result_ref))
     logger.info("Error E[%g, %g]: %.12e", alpha, beta, error)
     assert error < 2.0e-15
+
+
+# }}}
+
+
+# {{{ test_mittag_leffler_diethelm
 
 
 @pytest.mark.parametrize(
@@ -55,14 +65,12 @@ def test_mittag_leffler_series(alpha: float, beta: float) -> None:
     ],
 )
 def test_mittag_leffler_diethelm(alpha: float, beta: float) -> None:
-    from pycaputo.mittagleffler import mittag_leffler, mittag_leffler_diethelm
-
     rng = np.random.default_rng(seed=42)
     z = rng.random(128)
 
-    result_ref = mittag_leffler(z, alpha=alpha, beta=beta)
+    result_ref = ml.mittag_leffler(z, alpha=alpha, beta=beta)
     result = np.vectorize(
-        lambda zi: 0j + mittag_leffler_diethelm(zi, alpha=alpha, beta=beta)
+        lambda zi: 0j + ml.mittag_leffler_diethelm(zi, alpha=alpha, beta=beta)
     )(z)
 
     error = np.linalg.norm(result - result_ref) / (1 + np.linalg.norm(result_ref))
@@ -70,6 +78,34 @@ def test_mittag_leffler_diethelm(alpha: float, beta: float) -> None:
 
     # NOTE: due to the numerical quadrature, this can fail sometimes
     assert error < 2.0e-11
+
+
+# }}}
+
+
+# {{{
+
+
+@pytest.mark.parametrize("iref", [0, 1, 2, 3, 4])
+@pytest.mark.parametrize("alg", [ml.Algorithm.Series, ml.Algorithm.Diethelm])
+def test_mittag_leffler_mathematica(
+    iref: int, alg: ml.Algorithm, *, visualize: bool = True
+) -> None:
+    from mittag_leffler_ref import MATHEMATICA_RESULTS
+
+    ref = MATHEMATICA_RESULTS[iref]
+    is_on_unit_disk = np.all(np.abs(ref.z) <= 1.0)
+
+    if alg == ml.Algorithm.Series and not is_on_unit_disk:
+        pytest.skip("Series representation is not valid for z >= 1")
+
+    result = ml.mittag_leffler(ref.z, alpha=ref.alpha, beta=ref.beta)
+    error = np.linalg.norm(result - ref.result) / np.linalg.norm(ref.result)
+    logger.info("Error E[%g, %g]: %.12e", ref.alpha, ref.beta, error)
+    assert error < 1.0e-5
+
+
+# }}}
 
 
 if __name__ == "__main__":
