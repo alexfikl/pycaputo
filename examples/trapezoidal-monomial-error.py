5f85e2a60b148966651448dfb81ca0dccb2cda15
Alexandru Fikl <alexfikl@gmail.com>
2024-05-19 11:13:29 +0200

    feat(examples): use special solutions module

--- examples/trapezoidal-monomial-error.py
+++ examples/trapezoidal-monomial-error.py
@@ -2,12 +2,11 @@
 # SPDX-License-Identifier: MIT
 
 from dataclasses import dataclass
-from functools import partial
 
 import numpy as np
 import numpy.linalg as la
 
-from pycaputo.fode import caputo
+from pycaputo.fode import caputo, special
 from pycaputo.logging import get_logger
 from pycaputo.utils import Array, BlockTimer, StateFunctionT, gamma
 
@@ -20,60 +19,6 @@ class Trapezoidal(caputo.Trapezoidal[StateFunctionT]):
     def _get_kwargs(self, *, scalar: bool = True) -> dict[str, object]:
         return {"method": "newton", "xtol": 1.0e-12, "rtol": 1.0e-12}
 
-
-# {{{ function
-
-
-def func_y(t: float, *, t0: float, alpha: float, Yv: Array, nu: Array) -> Array:
-    result = np.sum(Yv * (t - t0) ** nu)
-    return np.array([result])
-
-
-def func_dy(t: float, *, t0: float, alpha: float, Yv: Array, nu: Array) -> Array:
-    mask = nu > 0.0
-    gYv = gamma(1 + nu) / gamma(1 + nu - alpha) * Yv
-    result = np.sum(gYv[mask] * (t - t0) ** (nu[mask] - alpha))
-
-    return np.array([result])
-
-
-def func_f(
-    t: float,
-    y: Array,
-    *,
-    t0: float,
-    alpha: float,
-    Yv: Array,
-    nu: Array,
-    beta: float,
-    c: float = 1.0,
-) -> Array:
-    y_ref = func_y(t, t0=t0, alpha=alpha, Yv=Yv, nu=nu)
-    dy_ref = func_dy(t, t0=t0, alpha=alpha, Yv=Yv, nu=nu)
-
-    result = dy_ref + c * (y_ref**beta - y**beta)
-    return np.array(result)
-
-
-def func_f_jac(
-    t: float,
-    y: Array,
-    *,
-    t0: float,
-    alpha: float,
-    Yv: Array,
-    nu: Array,
-    beta: float,
-    c: float = 1.0,
-) -> Array:
-    if c == 0:
-        return np.zeros_like(y)
-
-    result = -c * beta * y ** (beta - 1.0)
-    return result
-
-
-# }}}
 
 # {{{ solution
 
@@ -92,10 +37,18 @@ nu = np.array([i + alpha * j for i in range(nns) for j in range(nns)])
 nu = np.sort(nu[nu <= 2.0 + alpha])
 Yv = rng.uniform(0.0, 1.0, size=nu.size)
 
-# construct initial problem
-kwargs = {"t0": tstart, "alpha": alpha, "Yv": Yv, "nu": nu}
-solution = partial(func_y, **kwargs)
-y0 = solution(tstart)
+nustar = 3
+func_ref = special.CaputoMonomial(
+    Yv=Yv, nu=nu, t0=tstart, alpha=alpha, beta=beta, c=1.0
+)
+func_star = special.CaputoMonomial(
+    Yv=np.array([Yv[nustar]]),
+    nu=np.array([nu[nustar]]),
+    t0=tstart,
+    alpha=alpha,
+    beta=beta,
+    c=0.0,
+)
 
 # }}}
 
@@ -122,22 +75,18 @@ else:
 im_stepper = Trapezoidal(
     derivative_order=(alpha,),
     control=c,
-    source=partial(func_f, **kwargs, beta=beta),
-    source_jac=partial(func_f_jac, **kwargs, beta=beta),
-    y0=(y0,),
+    source=func_ref.source,
+    source_jac=func_ref.source_jac,
+    y0=(func_ref(tstart),),
 )
 
 # Implicit stepper for `(t - t0)^{nu^star}` where `nu^star = 2 alpha`
-nustar = 3
-kwargs = {**kwargs, "Yv": np.array([Yv[nustar]]), "nu": np.array([nu[nustar]])}
-en_solution = partial(func_y, **kwargs)
-
 en_stepper = Trapezoidal(
     derivative_order=(alpha,),
     control=c,
-    source=partial(func_f, **kwargs, beta=beta, c=0.0),
-    source_jac=partial(func_f_jac, **kwargs, beta=beta, c=0.0),
-    y0=(en_solution(tstart),),
+    source=func_star.source,
+    source_jac=func_star.source_jac,
+    y0=(func_star(tstart),),
 )
 
 from pycaputo.events import StepCompleted
@@ -169,12 +118,12 @@ with BlockTimer("evolve") as bt:
 
         # compute exact solution
         t_n = im_event.t
-        y_ref = solution(t_n)
+        y_ref = func_ref(t_n)
 
         # compute error model
         dtmax = max(dtmax, im_event.dt)
 
-        En = np.abs(en_event.y - en_solution(en_event.t)).item()
+        En = np.abs(en_event.y - func_star(en_event.t)).item()
         En_ref = (
             0.0
             if en_event.iteration == 0
@@ -255,7 +204,7 @@ en_ref = np.array(en_ref_l).T
 error = np.array(error_l).T
 mask = np.s_[1:]
 
-y_en_ref = np.array([en_solution(t_j) for t_j in t])
+y_en_ref = np.array([func_ref(t_j) for t_j in t])
 
 if isinstance(c, RandomController):
     with figure(f"{basename}-timestep") as fig:
