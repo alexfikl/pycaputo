9a6f54c59f4e5083127cd2fcac6873e601c27ed9
Alexandru Fikl <alexfikl@gmail.com>
2026-08-24 10:33:59 +0300

    feat: add better array typing

--- src/pycaputo/typing.py
+++ src/pycaputo/typing.py
@@ -11,12 +11,11 @@ from typing import (
     ParamSpec,
     Protocol,
     TypeAlias,
-    TypeVar,
     runtime_checkable,
 )
 
 import numpy as np
-from typing_extensions import TypeIs
+from typing_extensions import TypeAliasType, TypeIs, TypeVar
 
 if TYPE_CHECKING:
     from dataclasses import Field
@@ -51,17 +50,63 @@ Float: TypeAlias = int | float | np.integer[Any] | np.floating[Any]
 # {{{ numpy
 
 
-Array = np.ndarray[tuple[int, ...], np.dtype[np.floating[Any]]]
-"""Array type alias for a floating point :class:`numpy.ndarray`."""
-IntegerArray = np.ndarray[tuple[int, ...], np.dtype[np.integer[Any]]]
-"""Array type alias for an integer :class:`numpy.ndarray`."""
-Scalar = np.number[Any] | np.ndarray[tuple[()], np.dtype[np.number[Any]]]
+# TODO: Should probably just depend on `optype`. We already pull it in through
+# `scipy-stubs`. For now this is a very minimalist copy-paste from there. It
+# contains parts of
+#   optype/numpy/_array.py
+#   optype/numpy/_scalar.py
+
+ShapeT = TypeVar("ShapeT", bound=tuple[int, ...], default=tuple[Any, ...])
+"""An invariant type alias for ``tuple[int, ...]``."""
+ScalarTypeT = TypeVar("ScalarTypeT", bound=np.generic, default=Any)
+"""An invariant type alias for :mod:`numpy` scalars."""
+
+ArrayND = TypeAliasType(
+    "ArrayND",
+    np.ndarray[ShapeT, np.dtype[ScalarTypeT]],
+    type_params=(ScalarTypeT, ShapeT),
+)
+"""A type alias for a shape and type generic :class:`numpy.ndarray`."""
+
+Array0D = TypeAliasType(
+    "Array0D",
+    np.ndarray[tuple[()], np.dtype[ScalarTypeT]],
+    type_params=(ScalarTypeT,),
+)
+"""A type alias for a 0-dimensional :class:`ArrayND`."""
+
+Array1D = TypeAliasType(
+    "Array1D",
+    np.ndarray[tuple[int], np.dtype[ScalarTypeT]],
+    type_params=(ScalarTypeT,),
+)
+"""A type alias for a 1-dimensional :class:`ArrayND`."""
+
+Array2D = TypeAliasType(
+    "Array2D",
+    np.ndarray[tuple[int, int], np.dtype[ScalarTypeT]],
+    type_params=(ScalarTypeT,),
+)
+"""A type alias for a 2-dimensional :class:`ArrayND`."""
+
+Array3D = TypeAliasType(
+    "Array3D",
+    np.ndarray[tuple[int, int, int], np.dtype[ScalarTypeT]],
+    type_params=(ScalarTypeT,),
+)
+
+# TODO: we probably want to also support complex "scalars" down the road
+
+Scalar: TypeAlias = int | float | np.floating[Any]
 """Scalar type alias (generally a value convertible to a :class:`float`)."""
+ScalarLike: TypeAlias = Scalar | Array0D[np.number[Any]]
+"""A scalar-like value, which may include array of shape ``()``."""
 
-
-def is_array(x: object) -> TypeIs[Array]:
-    return isinstance(x, np.ndarray) and x.dtype.char != "O"
-
+# NOTE: these are very deprecated and should not be used
+Array: TypeAlias = ArrayND[np.floating[Any]]
+"""Array type alias for a floating point :class:`numpy.ndarray`."""
+IntegerArray: TypeAlias = ArrayND[np.integer[Any]]
+"""Array type alias for an integer :class:`numpy.ndarray`."""
 
 # }}}
 
@@ -89,7 +134,9 @@ class ScalarFunction(Protocol):
     .. automethod:: __call__
     """
 
-    def __call__(self, x: Array, /) -> Array:
+    def __call__(
+        self, x: ArrayND[ScalarTypeT, ShapeT], /
+    ) -> ArrayND[ScalarTypeT, ShapeT]:
         """
         :arg x: a scalar or array at which to evaluate the function.
         """
@@ -102,7 +149,9 @@ class DifferentiableScalarFunction(Protocol):
     .. automethod:: __call__
     """
 
-    def __call__(self, x: Array, /, d: int = 0) -> Array:
+    def __call__(
+        self, x: ArrayND[ScalarTypeT, ShapeT], /, d: int = 0
+    ) -> ArrayND[ScalarTypeT, ShapeT]:
         """Evaluate the function or any of its derivatives.
 
         :arg x: a scalar or array at which to evaluate the function.
@@ -110,7 +159,9 @@ class DifferentiableScalarFunction(Protocol):
         """
 
 
-ArrayOrScalarFunction = Array | ScalarFunction | DifferentiableScalarFunction
+ArrayOrScalarFunction = (
+    ArrayND[np.floating[Any]] | ScalarFunction | DifferentiableScalarFunction
+)
 """A union of scalar functions."""
 
 
@@ -134,7 +185,9 @@ class StateFunction(Protocol):
     .. automethod:: __call__
     """
 
-    def __call__(self, t: float, y: Array, /) -> Array:
+    def __call__(
+        self, t: float, y: ArrayND[ScalarTypeT, ShapeT], /
+    ) -> ArrayND[ScalarTypeT, ShapeT]:
         """
         :arg t: time at which to evaluate the function.
         :arg y: state vector value at which to evaluate the function.
@@ -149,7 +202,7 @@ class ScalarStateFunction(Protocol):
     .. automethod:: __call__
     """
 
-    def __call__(self, t: float, y: Array, /) -> float:
+    def __call__(self, t: float, y: ArrayND[ScalarTypeT, ShapeT], /) -> float:
         """
         :arg t: time at which to evaluate the function.
         :arg y: state vector value at which to evaluate the function.
